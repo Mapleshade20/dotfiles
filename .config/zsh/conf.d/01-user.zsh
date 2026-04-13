fd344f9c56a75277cec2e3048fd42f6abafe93d9
MapleShade20 <howard380mc@outlook.com>
2026-04-13 17:43:42 +0800

    refactor(zsh): move PATH and directory variables to zshenv-loaded file and check existence

--- .config/zsh/conf.d/01-user.zsh
+++ .config/zsh/conf.d/01-user.zsh
@@ -9,15 +9,34 @@ export RUFF_CACHE_DIR="$XDG_CACHE_HOME/ruff"
 export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
 export _JAVA_OPTIONS=-Djavafx.cachedir="$XDG_CACHE_HOME/openjfx"
 export CRYPTOGRAPHY_OPENSSL_NO_LEGACY=1
+export CLAUDE_CONFIG_DIR="$XDG_CONFIG_HOME/claude"
 
-export ZSH="$ZDOTDIR/oh-my-zsh"
-
+export DENO_INSTALL_ROOT="$XDG_DATA_HOME/deno/bin"
+export BUN_INSTALL="$XDG_DATA_HOME/bun" 
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+export NVM_DIR="$XDG_DATA_HOME/nvm"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 export GOPATH="$XDG_DATA_HOME/go"
 export GOMODCACHE="$XDG_CACHE_HOME/go/mod"
 
+export ZSH="$ZDOTDIR/oh-my-zsh"
 export EDITOR="nvim"
 export FD="fd"
 
-export PATH="$PATH:$GOPATH/bin:$CARGO_HOME/bin"
+typeset -U path PATH
+local extra_paths=(
+    "$GOPATH/bin"
+    "$CARGO_HOME/bin"
+    "$HOME/.local/bin"
+    "$XDG_DATA_HOME/npm/bin"
+    "$DENO_INSTALL_ROOT"
+    "$BUN_INSTALL/bin"
+    "/opt/homebrew/opt/make/libexec/gnubin"
+    "/opt/homebrew/opt/postgresql@18/bin"
+)
+for p in $extra_paths; do
+    [[ -d "$p" ]] && path=("$p" $path)
+done
+
+export PATH
