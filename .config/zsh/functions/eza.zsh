a79b9c5c800186bd477db21a2c330a7b65437629
MapleShade20 <howard380mc@outlook.com>
2026-05-10 21:14:58 +0800

    refactor(zsh)!: organize function definitions and port some of HyDE features

---
+++ .config/zsh/functions/eza.zsh
@@ -0,0 +1,7 @@
+if command -v "eza" &>/dev/null; then
+    alias l='eza -lh --icons=auto' \
+        ls='eza' \
+        ll='eza -lha --icons=auto --sort=name --group-directories-first' \
+        ld='eza -lhD --icons=auto' \
+        lt='eza --icons=auto --tree'
+fi
